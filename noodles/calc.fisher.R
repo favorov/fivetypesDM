4b245052bdc0fcedf1731ab5860a564aed43c525
Alexander Favorov <favorov@sensi.org>
2015-12-01 05:39:57 -0500

    p.values counter

---
+++ noodles/calc.fisher.R
@@ -0,0 +1,14 @@
+if(! 'noodles.M.methylation' %in% ls()) load('5types.meth.data.Rda')
+noodles.M.p.values<-apply(
+	noodles.M.methylation[1:1000,],1,
+	function(met) 
+	{
+		t<-table(typenames,as.logical(met))
+		if(dim(t)[2]==2)
+			fisher.test(t)$p.value
+		else
+			1.
+	}
+)
+save(file='5types.p.values.Rda',list=c('noodles.M.p.values'))
+
