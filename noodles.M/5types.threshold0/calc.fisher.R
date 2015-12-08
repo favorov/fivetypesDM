7127ff66960ba2f5625e0bbf5464d42e45262561
Alexander Favorov <favorov@sensi.org>
2015-12-08 05:51:56 -0500

    merge significant test and fisher test

--- noodles.M/5types.threshold0/calc.fisher.R
+++ noodles.M/5types.threshold0/calc.fisher.R
@@ -32,3 +32,10 @@ if(!noodles.M.p.values.loaded)
 	)
 	save(file='p.values.Rda',list=c('noodles.M.p.values','typenames'))
 }
+source('calc.fisher.R')
+#it will load or calc everything
+significant.bonf.corr<-which(noodles.M.p.values*length(noodles.M.p.values)<0.05)
+noodles.M.significant.bonf.corr<-noodles.M[significant.bonf.corr]
+noodles.M.methylation.significant.bonf.corr<-noodles.M.methylation[significant.bonf.corr,]
+noodles.M.p.values.significant.bonf.corr<-noodles.M.p.values[significant.bonf.corr]
+save(file='noodles.M.significant.bonf.corr.Rda',list=c('noodles.M.significant.bonf.corr','noodles.M.methylation.significant.bonf.corr','noodles.M.p.values.significant.bonf.corr'))
--- noodles.M/significant.probes.list.R
+++
@@ -1,7 +0,0 @@
-source('calc.fisher.R')
-#it will load or calc everything
-significant.bonf.corr<-which(noodles.M.p.values*length(noodles.M.p.values)<0.05)
-noodles.M.significant.bonf.corr<-noodles.M[significant.bonf.corr]
-noodles.M.methylation.significant.bonf.corr<-noodles.M.methylation[significant.bonf.corr,]
-noodles.M.p.values.significant.bonf.corr<-noodles.M.p.values[significant.bonf.corr]
-save(file='noodles.M.significant.bonf.corr.Rda',list=c('noodles.M.significant.bonf.corr','noodles.M.methylation.significant.bonf.corr','noodles.M.p.values.significant.bonf.corr'))
