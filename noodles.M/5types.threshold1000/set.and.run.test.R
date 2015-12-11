a71b836bb1f9a81cc7abae944d5207d030ae912b
Alexander Favorov <favorov@sensi.org>
2015-12-11 09:44:56 -0500

    two more folders to work

---
+++ noodles.M/5types.threshold1000/set.and.run.test.R
@@ -0,0 +1,9 @@
+source('../prepare.data.R')
+if(! 'noodles.M.methylation' %in% ls()) stop('data is not prepared!')
+
+threshold<-1000;
+test.typenames<-typenames;
+
+source('../calc.significance.R ')
+source('../PCA.R ')
+source('../boruta.R ')
