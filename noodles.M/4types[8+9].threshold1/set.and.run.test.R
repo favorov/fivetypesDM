0aa79a041c2843ad3558c33e1bd7dcb189cd04df
Alexander Favorov <favorov@sensi.org>
2015-12-15 04:44:14 -0500

    merge 8 and 9

---
+++ noodles.M/4types[8+9].threshold1/set.and.run.test.R
@@ -0,0 +1,10 @@
+source('../prepare.data.R')
+if(! 'noodles.M.methylation' %in% ls()) stop('data is not prepared!')
+
+threshold<-1; #inclusive
+test.typenames<-typenames;
+test.typenames[test.typenames=='T8' | test.typenames=='T9']='T89'
+
+source('../calc.significance.R')
+source('../PCA.R')
+source('../boruta.R')
