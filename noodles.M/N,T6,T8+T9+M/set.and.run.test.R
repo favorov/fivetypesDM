e205190cc15d1c4e457914c51dbd72cd1e9c868e
Alexander Favorov <favorov@sensi.org>
2015-12-17 04:14:33 -0500

    added three more classifications

---
+++ noodles.M/N,T6,T8+T9+M/set.and.run.test.R
@@ -0,0 +1,13 @@
+source('../prepare.data.R')
+if(! 'noodles.M.methylation' %in% ls()) stop('data is not prepared!')
+
+threshold<-1; #inclusive
+test.typenames<-typenames;
+test.typenames[test.typenames=='M' | test.typenames=='T8' | test.typenames=='T9']='MT89'
+
+Scol=c('red','blue','green','magenta','orange')
+names(Scol)<-unique(test.typenames)
+
+source('../calc.significance.R')
+source('../PCA.R')
+source('../boruta.R')
