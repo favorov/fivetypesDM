9e8e7357c94f6c28700f919e4d7c4ef776665a47
Alexander Favorov <favorov@sensi.org>
2015-12-18 05:20:48 -0500

    noodles and genes report for selected

---
+++ noodles.M/boruta.features.report.R
@@ -0,0 +1,9 @@
+library(differential.coverage)
+load(file = 'boruta.selected.Rda')
+sink('boruta.selected.probes.txt')
+print(as.data.frame(closest.gene.start.by.interval(noodles = boruta.selected.probes)))
+sink()
+sink('boruta.selected.probes.txt')
+print(as.data.frame(closest.gene.start.by.interval(noodles = boruta.bin.selected.probes)))
+sink()
+
