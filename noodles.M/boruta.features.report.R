9835af6abef74521b0dc9c78df42bf542421c695
Alexander Favorov <favorov@sensi.org>
2015-12-31 18:46:51 -0500

    merged with boruta.R\

--- noodles.M/boruta.features.report.R
+++
@@ -1,9 +0,0 @@
-library(differential.coverage)
-load(file = 'boruta.selected.Rda')
-sink('boruta.selected.probes.txt')
-print(as.data.frame(closest.gene.start.by.interval(noodles = boruta.selected.probes)))
-sink()
-sink('boruta.bin.selected.probes.txt')
-print(as.data.frame(closest.gene.start.by.interval(noodles = boruta.bin.selected.probes)))
-sink()
-
