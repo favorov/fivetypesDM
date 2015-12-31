bf20edbfcacea9d1c290d71c0a726a4eaf4c80e4
Alexander Favorov <favorov@sensi.org>
2015-12-30 23:22:55 -0500

    typo in uotput filename fixed

--- noodles.M/boruta.features.report.R
+++ noodles.M/boruta.features.report.R
@@ -3,7 +3,7 @@ load(file = 'boruta.selected.Rda')
 sink('boruta.selected.probes.txt')
 print(as.data.frame(closest.gene.start.by.interval(noodles = boruta.selected.probes)))
 sink()
-sink('boruta.selected.probes.txt')
+sink('boruta.bin.selected.probes.txt')
 print(as.data.frame(closest.gene.start.by.interval(noodles = boruta.bin.selected.probes)))
 sink()
 
