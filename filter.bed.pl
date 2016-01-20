cba36fec4370f242d7b303048761d1350f894646
Alexander Favorov <favorov@sensi.org>
2016-01-20 15:17:48 -0500

    $level var

--- filter.bed.pl
+++ filter.bed.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl -w
-#we remove all lines with fields ~=5 p,value (field 5) >1000
+#we remove all lines with fields ~=5 p,value (field 5) >$level
+$level=2000
 while(<>){
 	@a=split;
-	print if (4==$#a && $a[4]<=1000);
+	print if (4==$#a && $a[4]<=$level);
 }
